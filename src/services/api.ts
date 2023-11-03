494f1f6d417b3256bec8b222dc746b5de8f37a1f
matias.torres <mtri21019@gmail.com>
2023-11-03 15:06:54 -0300

    add route api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchData = async (url: string): Promise<any> => {
   try {
-    const response = await axios.get(`http://localhost:3000/api${url}`);
+    const response = await axios.get(`https://order-management-back.vercel.app/api${url}`);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching data from URL: ${url}`);
